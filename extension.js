8f06aa785fadc30fa6b5698496741c70cfe8eb7c
kechenhh <923407526@qq.com>
2022-11-05 14:01:00 +0800

    可读取

--- extension.js
+++ extension.js
@@ -1,34 +1,80 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
+const dir = 'D:\\zh.json';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "firstvs" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('firstvs.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from firstvs!');
-	});
-
-	context.subscriptions.push(disposable);
+function read(dir) {
+	return new Promise((resolve, reject) => {
+		fs.readFile(dir, 'utf-8', (err, data) => {
+			if (err) {
+				vscode.window.showInformationMessage('D盘下存放zh.json');
+				return
+			} else {
+				resolve(data)
+			}
+		})
+	})
+}
+//value 获取key
+function findKeyByValue(obj, value) {
+	for (let key in obj) {
+		for (let childkey in obj[key]) {
+			if (obj[key][childkey] === value) {
+				return `${key}.${childkey}`
+			}
+		}
+	}
+	return null
 }
 
-// This method is called when your extension is deactivated
-function deactivate() {}
+function activate(context) {
+
+	let disposable1 = vscode.commands.registerCommand("firstvs.transHTML", async function () {
+		//获取文本
+		const text = await read(dir)
+		const { activeTextEditor } = vscode.window // 获取当前聚焦的文本编辑器
+		// activeTextEditor!.selection 当前选中的范围
+		const currentSelect = activeTextEditor.document.getText(activeTextEditor.selection) // 根据范围获取选中文本
+		//json  to obj
+		let obj = JSON.parse(text)
+		let transWord = findKeyByValue(obj, currentSelect)
+
+		if (transWord) {
+			let pushword = `$t("${transWord}")`
+			activeTextEditor.edit(editBuilder => {
+				editBuilder.replace(activeTextEditor.selection, pushword)
+			})
+			vscode.window.showInformationMessage(currentSelect + '翻译html成功');
+		} else {
+			vscode.window.showInformationMessage(currentSelect + '匹配html失败');
+		}
+
+
+	});
+	let disposable2 = vscode.commands.registerCommand('firstvs.transJS', function () {
+
+		const { activeTextEditor } = vscode.window // 获取当前聚焦的文本编辑器
+		// activeTextEditor!.selection 当前选中的范围
+		const currentSelect = activeTextEditor.document.getText(activeTextEditor.selection) // 根据范围获取选中文本
+		//json to obj
+		let obj = JSON.parse(jsonStr)
+		let transWord = findKeyByValue(obj, currentSelect)
+		if (transWord) {
+			let pushword = `this.$t("${transWord}")`
+			activeTextEditor.edit(editBuilder => {
+				editBuilder.replace(activeTextEditor.selection, pushword)
+			})
+			vscode.window.showInformationMessage(currentSelect + '翻译js成功');
+		} else {
+			vscode.window.showInformationMessage(currentSelect + '匹配js失败');
+		}
+
+
+	});
+	context.subscriptions.push(disposable1, disposable2);
+}
+
+function deactivate() { }
 
 module.exports = {
 	activate,

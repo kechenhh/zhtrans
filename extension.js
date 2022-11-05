baa63174adfb52d022ae4fd5856ea2665b230be0
kechenhh <923407526@qq.com>
2022-11-05 15:40:16 +0800

    添加右键

--- extension.js
+++ extension.js
@@ -2,6 +2,7 @@ const vscode = require('vscode');
 const fs = require('fs');
 const dir = 'D:\\zh.json';
 
+//读取文本
 function read(dir) {
 	return new Promise((resolve, reject) => {
 		fs.readFile(dir, 'utf-8', (err, data) => {
@@ -14,6 +15,7 @@ function read(dir) {
 		})
 	})
 }
+
 //value 获取key
 function findKeyByValue(obj, value) {
 	for (let key in obj) {
@@ -25,50 +27,38 @@ function findKeyByValue(obj, value) {
 	}
 	return null
 }
+async function main(type) {
+	//获取文本
+	const text = await read(dir)
+	const { activeTextEditor } = vscode.window // 获取当前聚焦的文本编辑器
+	// 根据范围获取选中文本 // activeTextEditor.selection 当前选中的范围
+	const currentSelect = activeTextEditor.document.getText(activeTextEditor.selection)
+	//json  to obj
+	let obj = JSON.parse(text)
+	let transWord = findKeyByValue(obj, currentSelect)
+	//转换
+	if (transWord) {
+		let pushword = ''
+		if (type == 'html') {
+			pushword = `$t("${transWord}")`
+		} else if (type == 'js') {
+			pushword = `this.$t("${transWord}")`
+		}
+		activeTextEditor.edit(editBuilder => {
+			editBuilder.replace(activeTextEditor.selection, pushword)
+		})
+	}
+	let sendText = `${currentSelect} 翻译${transWord ? '成功' : '失败'}！`
+	vscode.window.showInformationMessage(sendText);
+}
 
 function activate(context) {
-
-	let disposable1 = vscode.commands.registerCommand("zhtrans.transHTML", async function () {
-		//获取文本
-		const text = await read(dir)
-		const { activeTextEditor } = vscode.window // 获取当前聚焦的文本编辑器
-		// activeTextEditor!.selection 当前选中的范围
-		const currentSelect = activeTextEditor.document.getText(activeTextEditor.selection) // 根据范围获取选中文本
-		//json  to obj
-		let obj = JSON.parse(text)
-		let transWord = findKeyByValue(obj, currentSelect)
-
-		if (transWord) {
-			let pushword = `$t("${transWord}")`
-			activeTextEditor.edit(editBuilder => {
-				editBuilder.replace(activeTextEditor.selection, pushword)
-			})
-			vscode.window.showInformationMessage(currentSelect + '翻译html成功');
-		} else {
-			vscode.window.showInformationMessage(currentSelect + '匹配html失败');
-		}
-
-
+	let disposable1 = vscode.commands.registerCommand("zhtrans.trh", function () {
+		main('html')
 	});
-	let disposable2 = vscode.commands.registerCommand('zhtrans.transJS', function () {
 
-		const { activeTextEditor } = vscode.window // 获取当前聚焦的文本编辑器
-		// activeTextEditor!.selection 当前选中的范围
-		const currentSelect = activeTextEditor.document.getText(activeTextEditor.selection) // 根据范围获取选中文本
-		//json to obj
-		let obj = JSON.parse(jsonStr)
-		let transWord = findKeyByValue(obj, currentSelect)
-		if (transWord) {
-			let pushword = `this.$t("${transWord}")`
-			activeTextEditor.edit(editBuilder => {
-				editBuilder.replace(activeTextEditor.selection, pushword)
-			})
-			vscode.window.showInformationMessage(currentSelect + '翻译js成功');
-		} else {
-			vscode.window.showInformationMessage(currentSelect + '匹配js失败');
-		}
-
-
+	let disposable2 = vscode.commands.registerCommand('zhtrans.trj', function () {
+		main('js')
 	});
 	context.subscriptions.push(disposable1, disposable2);
 }

c855def11f34e856850fd9150c302b1adaa3de47
kechenhh <923407526@qq.com>
2022-11-05 19:41:07 +0800

    0.3随机拼音转换

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
 const vscode = require('vscode');
 const fs = require('fs');
 const dir = vscode.workspace.getConfiguration('zhtrans').get('readDir')
+const { pinyin } = require('pinyin-pro');
+const { activeTextEditor } = vscode.window // 获取当前聚焦的文本编辑器
 
 //读取文本
 function read(dir) {
@@ -27,30 +29,55 @@ function findKeyByValue(obj, value) {
 	}
 	return null
 }
+
+//拼音随机生成
+function randomTxt(str) {
+	let pinyinArr = pinyin(str, { pattern: 'initial', type: 'array' });
+	let tran = pinyinArr.join('').slice(0, 6)
+	return tran
+}
+
+//转换文字
+function transTxt(type, isMatch, word) {
+	let pushword = ''
+	if (type == 'html') {
+		pushword = `{{$t("${isMatch ? word : randomTxt(word)}")}}`
+	} else if (type == 'js') {
+		pushword = `this.$t("${isMatch ? word : randomTxt(word)}")`
+	}
+	activeTextEditor.edit(editBuilder => {
+		editBuilder.replace(activeTextEditor.selection, pushword)
+	})
+}
+
 async function main(type) {
 	//获取文本
 	const text = await read(dir)
-	const { activeTextEditor } = vscode.window // 获取当前聚焦的文本编辑器
 	// 根据范围获取选中文本 // activeTextEditor.selection 当前选中的范围
 	let currentSelect = activeTextEditor.document.getText(activeTextEditor.selection)
+
 	currentSelect = currentSelect.replace(/"/g, '').replace(/'/g, '').trim()
 	//json  to obj
 	let obj = JSON.parse(text)
 	let transWord = findKeyByValue(obj, currentSelect)
+	//随机转换写入状态
+	const freeTrans = vscode.workspace.getConfiguration('zhtrans').get('freeTrans') || false
+	let sendText = ''
 	//转换
 	if (transWord) {
-		let pushword = ''
-		if (type == 'html') {
-			pushword = `{{$t("${transWord}")}}`
-		} else if (type == 'js') {
-			pushword = `this.$t("${transWord}")`
+		//匹配成功
+		transTxt(type, true, transWord)
+		sendText = `${currentSelect} 翻译成功！`
+	} else {
+		if (freeTrans) {
+			transTxt(type, false, currentSelect)
+			sendText = `${currentSelect} 随机！`
+		} else {
+			sendText = `${currentSelect} 翻译失败！`
 		}
-		activeTextEditor.edit(editBuilder => {
-			editBuilder.replace(activeTextEditor.selection, pushword)
-		})
 	}
-	let sendText = `${currentSelect} 翻译${transWord ? '成功' : '失败'}！`
 	vscode.window.showInformationMessage(sendText);
+
 }
 
 function activate(context) {
